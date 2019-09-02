950147b0151ce20412cc0e6c6fc26b16e7f7542a
evansibok <evansibok@gmail.com>
2019-09-02 14:39:31 +0100

    p tags selected

--- js/index.js
+++ js/index.js
@@ -83,19 +83,13 @@ let h4 = document.querySelectorAll("h4");
 
 // h4 Elements contents added
 h4[0].textContent = siteContent["main-content"]["features-h4"];
-
 h4[1].textContent = siteContent["main-content"]["about-h4"];
-
-
 h4[2].textContent = siteContent["main-content"]["services-h4"];
-
-
 h4[3].textContent = siteContent["main-content"]["product-h4"];
-
-
 h4[4].textContent = siteContent["main-content"]["vision-h4"];
 h4[5].textContent = siteContent["contact"]["contact-h4"];
 
+// Paragraphs tags selection
 let p = document.querySelectorAll("p");
 p[0].textContent = siteContent["main-content"]["features-content"];
 
