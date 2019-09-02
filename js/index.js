a1f7294435ed1c44941587f99c4452d50c8600b7
evansibok <evansibok@gmail.com>
2019-09-02 14:33:49 +0100

    start adding comments

--- js/index.js
+++ js/index.js
@@ -42,6 +42,8 @@ let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"]);
 
 // Solutions
+
+// Nav Logo
 let navImage = document.getElementById('logo-img');
 navImage.setAttribute('src', siteContent["nav"]['img-src']);
 
@@ -58,6 +60,10 @@ navLinks[2].textContent = siteContent["nav"]["nav-item-3"];
 navLinks[3].textContent = siteContent["nav"]["nav-item-4"];
 navLinks[4].textContent = siteContent["nav"]["nav-item-5"];
 navLinks[5].textContent = siteContent["nav"]["nav-item-6"];
+navLinks.classList.add("greenColor");
+
+let coloredLinks = document.getElementsByClassName("greenColor");
+coloredLinks.style.color = "green";
 
 let h1 = document.querySelector("h1");
 h1.textContent = siteContent["cta"]["h1"];
@@ -78,3 +84,24 @@ h4[3].textContent = siteContent["main-content"]["product-h4"];
 
 
 h4[4].textContent = siteContent["main-content"]["vision-h4"];
+h4[5].textContent = siteContent["contact"]["contact-h4"];
+
+let p = document.querySelectorAll("p");
+p[0].textContent = siteContent["main-content"]["features-content"];
+
+p[1].textContent = siteContent["main-content"]["about-content"];
+
+p[2].textContent = siteContent["main-content"]["product-content"];
+
+p[3].textContent = siteContent["main-content"]["vision-content"];
+
+p[4].textContent = siteContent["main-content"]["features-content"];
+
+p[5].textContent = siteContent["contact"]["address"];
+
+p[6].textContent = siteContent["contact"]["phone"];
+
+p[7].textContent = siteContent["contact"]["email"];
+
+p[8].textContent = siteContent["footer"]["copyright"];
+
